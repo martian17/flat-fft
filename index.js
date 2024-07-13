5adb04e83f218ef8fbd0d31c36a6a0c19526b27c
martian17 <yakumodanshi@gmail.com>
2024-07-13 10:24:58 +0200

    Add in-place transform

--- index.js
+++ index.js
@@ -53,6 +53,47 @@ export class FlatFFT32{
         }
         return buff;
     }
+    baseInPlaceTransform(coefs){
+        const coefs64 = new Float64Array(coefs.buffer);
+        const {nummap,order} = this;
+        const n = this.length;
+        for(let i = 0; i < n; i++){
+            let idx = nummap[i];
+            if(i === idx || idx < i)continue;
+            if(nummap[idx] !== i)throw new Error("Whats happening?");
+            // we exchange these two numbers
+            //copying the real and imaginary together as 64 bit double
+            let val = coefs64[i];
+            coefs64[i] = coefs64[idx];
+            coefs64[idx] = val;
+        }
+        for(let o = 1; o <= order; o++){
+            const span = 1<<o;
+            const hspan = span>>>1;//half span
+            for(let offset = 0; offset < n; offset += span){
+                for(let i = 0; i < hspan; i++){
+                    let evenIdx = (offset+i)*2;
+                    let oddIdx = (offset+i+hspan)*2;
+                    //ith span root of unity
+                    const omega_r = Math.cos(i/span*Math.PI*2);
+                    const omega_i = Math.sin(i/span*Math.PI*2);
+                    const even0_r = coefs[evenIdx+0];
+                    const even0_i = coefs[evenIdx+1];
+                    const odd0_r  = coefs[oddIdx +0];
+                    const odd0_i  = coefs[oddIdx +1];
+
+                    const right_r = odd0_r*omega_r - odd0_i*omega_i;
+                    const right_i = odd0_r*omega_i + odd0_i*omega_r;
+
+                    coefs[evenIdx+0] = even0_r+right_r;
+                    coefs[evenIdx+1] = even0_i+right_i;
+                    coefs[oddIdx +0] = even0_r-right_r;
+                    coefs[oddIdx +1] = even0_i-right_i;
+                }
+            }
+        }
+        return coefs;
+    }
     fft(coefs, returnBuffer){
         const buff = this.baseTransform(coefs, returnBuffer);
         const n = this.length;
@@ -69,6 +110,23 @@ export class FlatFFT32{
             buff[i] = buff[i]/n;
         }
         return buff;
+    }
+    fftInPlace(coefs){
+        this.baseInPlaceTransform(coefs);
+        const n = this.length;
+        //taking the complex conjugate
+        for(let i = 1; i < n*2; i += 2){
+            coefs[i] = -coefs[i];
+        }
+        return coefs;
+    }
+    ifftInPlace(coefs){
+        this.baseInPlaceTransform(coefs);
+        const n = this.length;
+        for(let i = 0; i < n*2; i++){
+            coefs[i] = coefs[i]/n;
+        }
+        return coefs;
     }
     static toComplex(arr){
         const res = new Float32Array(arr.length*2);
@@ -124,6 +182,50 @@ export class FlatFFT64 extends FlatFFT32{
         }
         return buff;
     }
+    baseInPlaceTransform(coefs, returnBuffer){
+        const {nummap,order} = this;
+        const n = this.length;
+        // change the number arrangement
+        for(let i = 0; i < n; i++){
+            let idx = nummap[i];
+            if(i === idx || idx < i)continue;
+            if(nummap[idx] !== i)throw new Error("Whats happening?");
+            // we exchange these two numbers
+            const vr = coefs[i*2];
+            const vi = coefs[i*2+1];
+            //copying the real and imaginary together as 64 bit double
+            coefs[i*2+0] = coefs[idx*2+0];
+            coefs[i*2+1] = coefs[idx*2+1];
+            coefs[idx*2+0] = vr;
+            coefs[idx*2+1] = vi;
+        }
+        for(let o = 1; o <= order; o++){
+            const span = 1<<o;
+            const hspan = span>>>1;//half span
+            for(let offset = 0; offset < n; offset += span){
+                for(let i = 0; i < hspan; i++){
+                    let evenIdx = (offset+i)*2;
+                    let oddIdx = (offset+i+hspan)*2;
+                    //ith span root of unity
+                    const omega_r = Math.cos(i/span*Math.PI*2);
+                    const omega_i = Math.sin(i/span*Math.PI*2);
+                    const even0_r = coefs[evenIdx+0];
+                    const even0_i = coefs[evenIdx+1];
+                    const odd0_r  = coefs[oddIdx +0];
+                    const odd0_i  = coefs[oddIdx +1];
+
+                    const right_r = odd0_r*omega_r - odd0_i*omega_i;
+                    const right_i = odd0_r*omega_i + odd0_i*omega_r;
+
+                    coefs[evenIdx+0] = even0_r+right_r;
+                    coefs[evenIdx+1] = even0_i+right_i;
+                    coefs[oddIdx +0] = even0_r-right_r;
+                    coefs[oddIdx +1] = even0_i-right_i;
+                }
+            }
+        }
+        return coefs;
+    }
     static toComplex(arr){
         const res = new Float64Array(arr.length*2);
         for(let i = 0; i < arr.length; i++){
@@ -161,10 +263,20 @@ export const createFFTGetter = function(FlatFFT){
         const transformer = getFFT(arr.length/2);
         return transformer.ifft(arr);
     };
-    return {fft,ifft};
+
+    const fftInPlace = function(arr){
+        const transformer = getFFT(arr.length/2);
+        return transformer.fftInPlace(arr);
+    };
+    
+    const ifftInPlace = function(arr){
+        const transformer = getFFT(arr.length/2);
+        return transformer.ifftInPlace(arr);
+    };
+    return {fft,ifft,fftInPlace,ifftInPlace};
 };
 
-export const {fft: fft32, ifft: ifft32} = createFFTGetter(FlatFFT32);
-export const {fft, ifft} = createFFTGetter(FlatFFT64);
-export const {fft: fft64, ifft: ifft64} = createFFTGetter(FlatFFT64);
+export const {fft: fft32, ifft: ifft32, fftInPlace: fft32InPlace, ifftInPlace: ifft32InPlace} = createFFTGetter(FlatFFT32);
+export const {fft, ifft, fftInPlace, ifftInPlace} = createFFTGetter(FlatFFT64);
+export const {fft: fft64, ifft: ifft64, fftInPlace: fft64InPlace, ifftInPlace: ifft64InPlace} = createFFTGetter(FlatFFT64);
 export const FlatFFT = FlatFFT32;
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-import {FlatFFT32, FlatFFT64, fft32, ifft32, fft64, ifft64} from "./index";
+import {FlatFFT32, FlatFFT64, fft32, ifft32, fft64, ifft64, fft64InPlace, ifft64InPlace} from "./index";
 
 const round = function(arr){
     const res = [];
@@ -23,6 +23,16 @@ test("Flat64: fft->ifft should give back the same input", ()=>{
         round(ifft64(fft64(original)))
     ).toStrictEqual(
         round(original)
+    );
+});
+
+test("Flat64: fftInPlace->ifftInPlace should give back the same input", ()=>{
+    const original = FlatFFT64.toComplex([1,2,3,4]);
+    const original_copy = original.slice();
+    expect(
+        round(ifft64InPlace(fft64InPlace(original)))
+    ).toStrictEqual(
+        round(original_copy)
     );
 });
 
